d09ac8eb3fa3d64a9c8c057c86bf79df1a662291
Shivaharip <shivahari@qxf2.com>
2017-10-13 09:40:16 +0530

    Modifed ConnectJira to generate json files and Added html files that use the json files to plot

--- ConnectJira.py
+++ ConnectJira.py
@@ -9,6 +9,7 @@ import pandas as pd
 import re
 import arrow
 import matplotlib.pyplot as plt
+from time import strftime
 
 class ConnectJira():
     """
@@ -33,10 +34,15 @@ class ConnectJira():
                             da_update_date = action.created
                             now_date = arrow.now()
                             ticket_in_qa_time = now_date - arrow.get(da_update_date)
-                            print ticket,item.toString,ticket_in_qa_time
-                            status_qa_data_frame.loc[len(status_qa_data_frame)] = {'ticket-ID':ticket.key,'status':item.toString,'days':ticket_in_qa_time.days}
+                            ticket_in_qa_time = int(ticket_in_qa_time.days)
+                            status_qa_data_frame.loc[len(status_qa_data_frame)] = {'ticket-ID':ticket.key,'status':item.toString,'days':ticket_in_qa_time}
         self.status_qa_data_frame = status_qa_data_frame
         print self.status_qa_data_frame
+        json_data = {"x_axis":list(self.status_qa_data_frame['ticket-ID']),"y_axis":list(self.status_qa_data_frame['days'])}
+        json_data = "get_issue = '" + json.dumps(json_data) + "';"
+        with open('get_issue_in_qa.json','w') as outfile:
+            outfile.write(json_data)
+
 
     def get_reporter_list(self):
         "Get the list of JIRA ticket reporters"
@@ -49,6 +55,7 @@ class ConnectJira():
             reporter_list.append(reporter)
 
         return reporter_list
+
 
     def get_description_reporters_data_frame(self):
         "Get a dataframe of reporters and words used in description"
@@ -67,9 +74,11 @@ class ConnectJira():
                 verbose_data_frame.loc[len(verbose_data_frame)] = {'name':reporter,'avg_word_count':avg_words}
         self.verbose_data_frame = verbose_data_frame
         print self.verbose_data_frame
-        #self.plot_graph(verbose_data_frame['Reporter'],verbose_data_frame['Avg_word_count'])
-        
-        #print verbose_data_frame
+        json_data = {"x_axis":list(self.verbose_data_frame['name']),"y_axis":list(self.verbose_data_frame['avg_word_count'])}
+        json_data = "desc_word_count = '" + json.dumps(json_data) + "';"
+        with open('description_word_count.json','w') as outfile:
+            outfile.write(json_data)
+
 
     def get_comments(self):
         "Get the comments"
@@ -85,13 +94,17 @@ class ConnectJira():
         unique_comment_authors_data_frame['average'] = unique_comment_authors_data_frame['comment_word_count'] / (unique_comment_authors_data_frame['no_times']*1.0)
         self.unique_comment_authors_data_frame = unique_comment_authors_data_frame
         print self.unique_comment_authors_data_frame 
-        #print pd.merge(self.verbose_data_frame,self.unique_comment_authors_data_frame,on='name',how='right')
+        json_data = {"x_axis":self.unique_comment_authors_data_frame.index.tolist(),"y_axis":list(self.unique_comment_authors_data_frame['average'])}
+        json_data = "comm_word_count = '" + json.dumps(json_data) + "';"
+        with open('comment_word_count.json','w') as outfile:
+            outfile.write(json_data)
 
 
     def plot_graph(self,x_points,y_points):
         "Plot Graph using the X and Y axis passed"
         plt.plot(x_points,y_points)
         plt.show()
+
 
 if __name__ == '__main__':
     obj = ConnectJira('https://jira.secondlife.com','SUN')
